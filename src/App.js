684fc3a73e10647e4e1f8535eb04590f25a4a85a
Harry <hdhillon478@gmail.com>
2021-10-31 22:07:49 +0000

    Add initial useEffect to get pokemon data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
 const App = () => {
+  const [pokemonData, setPokemonData] = useState([]); // Set initial state to be empty
+  const [nextUrl, setNextUrl] = useState("");
+  const [prevUrl, setprevUrl] = useState("");
+  const [loading, setLoading] = useState(true); // Loading will be set to false when data is loaded
+
+  const initialUrl = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialUrl);
+    }
+  }, []); //  empty array means loading will only happen once
+
   return (
     <div className='container'>
       <Navbar />
