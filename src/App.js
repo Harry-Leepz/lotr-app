a186bd5ce7eccb1a94cd76c435ccc8db367da247
Harry <hdhillon478@gmail.com>
2021-10-31 22:22:02 +0000

    Add functionality to fetch data from pokemon api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { getAllPokemon } from "./components/data/Pokemon";
 import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
@@ -13,12 +14,18 @@ const App = () => {
   useEffect(() => {
     async function fetchData() {
       let response = await getAllPokemon(initialUrl);
+      console.log(response);
+      setNextUrl(response.next); // data from fetch request
+      setprevUrl(response.previous); // data from fetch request
+      setLoading(false);
     }
+    fetchData();
   }, []); //  empty array means loading will only happen once
 
   return (
     <div className='container'>
       <Navbar />
+      {loading ? <h1>Loading...</h1> : <h1> Data is here </h1>}
     </div>
   );
 };
