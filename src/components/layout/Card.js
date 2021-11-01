258be2254538c08351b98d1f98455b778be0d828
Harry <hdhillon478@gmail.com>
2021-11-01 10:12:44 +0000

    Buid card component with api info

--- src/components/layout/Card.js
+++ src/components/layout/Card.js
@@ -1,7 +1,35 @@
 import React from "react";
 
-const Card = () => {
-  return <div></div>;
+const Card = ({ pokemon }) => {
+  return (
+    <div className='Card'>
+      <div className='Card__img'>
+        <img src={pokemon.sprites.front_default} />
+      </div>
+      <div className='Card__Name'>{pokemon.name}</div>
+      <div className='Card__types'>
+        {pokemon.types.map((type) => {
+          return <div className='Card__type'>{type.type.name}</div>;
+        })}
+      </div>
+      <div className='Card_info'>
+        <div className='Card__data'>
+          <p>
+            <strong>Weight: </strong>
+            {pokemon.weight}
+          </p>
+          <p>
+            <strong>Height: </strong>
+            {pokemon.height}
+          </p>
+          <p>
+            <strong>Ability: </strong>
+            {pokemon.abilities[0].ability.name}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
